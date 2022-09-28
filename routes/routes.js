d332d0b00425a0eb3264cc229ca20b7f770f91ab
Felipe <1felipersilva1@gmail.com>
2022-09-27 21:51:52 -0300

    Delete task

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,11 @@
 const Router = require ("express")
-const {getAllTasks, createTask, getById, updateOneTask} = require ("../controller/TaskController.js")
+const {getAllTasks, createTask, getById, updateOneTask, deleteOneTask} = require ("../controller/TaskController.js")
 
 const router = Router();
 
 router.get('/home', getAllTasks)
 router.get('/home/getById/:id/:method', getById)
+router.get('/home/deleteOneTask/:id/', deleteOneTask)
 router.post('/home/create', createTask)
 router.post('/home/updateOne/:id', updateOneTask)
 
